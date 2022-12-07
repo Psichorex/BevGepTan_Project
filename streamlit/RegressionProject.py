8dace2e7d35339962de7de02e33c9357d29d7a9c
Roland Márkus <roland.markus@lombiq.com>
2022-12-07 10:33:02 +0100

    Adjusting files.

---
+++ streamlit/RegressionProject.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import numpy as np
+
+from sklearn.linear_model import Lasso
+
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
+
+import pandas as pd
+
+from sklearn.metrics import mean_squared_error
+
+import streamlit as st
+
+from sklearn.svm import SVR
+
+database = pd.read_csv('fifa_23_280922.csv') 
+
+database = database.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna(axis=1, how='all')
+
+database  = database.drop('Taille', axis = 1)
+
+database = database.drop('Id', axis = 1)
+
+X = database.drop('Potentiel', axis = 1)
+
+X.head()
+
+y = database['Potentiel']
+
+y.head()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state = 42)
+
+lasso = Lasso(max_iter=10000, tol= 0.001, alpha = 1.0)
+
+lasso.fit(X_train, y_train)
+
+y_pred = lasso.predict(X_test)
+
+y_pred
+
+crossVal = cross_validate(lasso, X, y, cv=10, return_train_score=True)
+
+print(crossVal)
+
+print(np.average(crossVal['train_score']))
+
+print(np.average(crossVal['test_score']))
+
+
