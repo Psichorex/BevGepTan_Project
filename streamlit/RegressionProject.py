b92ea8fb6e749d6dfbacf0f8df9ae4ec4916fc73
Roland Márkus <roland.markus@lombiq.com>
2022-12-07 14:35:41 +0100

    Fix

--- streamlit/RegressionProject.py
+++ streamlit/RegressionProject.py
@@ -31,7 +31,13 @@ database  = database.drop('Taille', axis = 1)
 
 database = database.drop('Id', axis = 1)
 
+database = database.drop('Mauvais pied', axis = 1)
+
+database = database.drop('Gestes techniques', axis = 1)
+
 X = database.drop('Potentiel', axis = 1)
+
+features = X.keys()
 
 X.head()
 
@@ -61,3 +67,52 @@ st.markdown('Train Score: ')
 st.markdown(np.average(crossVal['train_score']))
 st.markdown('Test Score: ')
 st.markdown(np.average(crossVal['test_score']))
+
+st.markdown('Try yourself')
+
+stat1 = st.slider('Stat', min_value=0, max_value=99, key=0)
+stat2 = st.slider('Stat', min_value=0, max_value=99, key=1)
+stat3 = st.slider('Stat', min_value=0, max_value=99, key=2)
+stat4 = st.slider('Stat', min_value=0, max_value=99, key=3)
+stat5 = st.slider('Stat', min_value=0, max_value=99, key=4)
+stat6 = st.slider('Stat', min_value=0, max_value=99, key=5)
+stat7 = st.slider('Stat', min_value=0, max_value=99, key=6)
+stat8 = st.slider('Stat', min_value=0, max_value=99, key=7)
+stat9 = st.slider('Stat', min_value=0, max_value=99, key=8)
+stat10 = st.slider('Stat', min_value=0, max_value=99, key=10)
+stat11 = st.slider('Stat', min_value=0, max_value=99, key=11)
+stat12 = st.slider('Stat', min_value=0, max_value=99, key=12)
+stat13 = st.slider('Stat', min_value=0, max_value=99, key=13)
+stat14 = st.slider('Stat', min_value=0, max_value=99, key=14)
+stat15 = st.slider('Stat', min_value=0, max_value=99, key=15)
+stat16 = st.slider('Stat', min_value=0, max_value=99, key=16)
+stat17 = st.slider('Stat', min_value=0, max_value=99, key=17)
+stat18 = st.slider('Stat', min_value=0, max_value=99, key=18)
+stat19 = st.slider('Stat', min_value=0, max_value=99, key=19)
+stat20 = st.slider('Stat', min_value=0, max_value=99, key=20)
+stat21 = st.slider('Stat', min_value=0, max_value=99, key=21)
+stat22 = st.slider('Stat', min_value=0, max_value=99, key=22)
+stat23 = st.slider('Stat', min_value=0, max_value=99, key=23)
+stat24 = st.slider('Stat', min_value=0, max_value=99, key=24)
+stat25 = st.slider('Stat', min_value=0, max_value=99, key=25)
+stat26 = st.slider('Stat', min_value=0, max_value=99, key=26)
+stat27 = st.slider('Stat', min_value=0, max_value=99, key=27)
+stat28 = st.slider('Stat', min_value=0, max_value=99, key=28)
+stat29 = st.slider('Stat', min_value=0, max_value=99, key=29)
+stat30 = st.slider('Stat', min_value=0, max_value=99, key=30)
+stat31 = st.slider('Stat', min_value=0, max_value=99, key=31)
+stat32 = st.slider('Stat', min_value=0, max_value=99, key=32)
+stat33 = st.slider('Stat', min_value=0, max_value=99, key=33)
+stat34 = st.slider('Stat', min_value=0, max_value=99, key=34)
+stat35 = st.slider('Stat', min_value=0, max_value=99, key=35)
+stat36 = st.slider('Stat', min_value=0, max_value=99, key=36)
+
+player = [stat1,stat2,stat3,stat4,stat5,stat6,stat7,stat8,stat9,stat10,
+stat11,stat12,stat13,stat14,stat15,stat16,stat17,stat18,stat19,stat20,
+stat21,stat22,stat23,stat24,stat25,stat26,stat27,stat28,stat29,stat30,
+stat31,stat32,stat33,stat34,stat35,stat36]
+
+array = np.array(player)
+df = pd.DataFrame(array.reshape(-1, len(array)), columns=features)
+playerPredict = lasso.predict(df)
+st.markdown(playerPredict)
